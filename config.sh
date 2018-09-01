4a5be4ad06a26ff22e325b413bcbc512e7c0dae9
RogerF81 <lowhouse123@gmail.com>
2018-09-01 12:30:30 +0200

    Updated Magisk module template

--- config.sh
+++ config.sh
@@ -40,9 +40,9 @@ LATESTARTSERVICE=true
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "**************************************"
-  ui_print "           HelixEngine Add-on         "
-  ui_print "**************************************"
+  ui_print "**********************************"
+  ui_print "        HelixEngine Add-on        "
+  ui_print "**********************************"
 }
 
 ##########################################################################################
