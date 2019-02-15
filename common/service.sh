403ebf4256e0efe7328078125802018e6ae57d0f
RogerF81 <lowhouse123@gmail.com>
2019-02-15 10:17:21 +0100

    Little optimizations

--- common/service.sh
+++ common/service.sh
@@ -10,17 +10,24 @@ sleep 20
 #Governor
 if grep "schedutil" /sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors; then
 	#LITTLE
-	echo 1000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
-	echo 5000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
-	echo 1 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/iowait_boost_enable
+	echo 750 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
+	echo 15000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/iowait_boost_enable
 	#big
-	echo 1000 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
-	echo 5000 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us
-	echo 1 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/iowait_boost_enable
+	echo 750 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
+	echo 15000 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us
+	echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/iowait_boost_enable
+	echo 0 /sys/module/cpu-boost/parameters/dynamic_stune_boost
+	echo 0:0 4:0 >/sys/module/cpu-boost/parameters/input_boost_freq
+	echo 0 >/sys/module/cpu-boost/parameters/input_boost_ms
+	echo 0 >/dev/stune/top-app/schedtune.boost
+	echo 5 >/dev/stune/top-app/schedtune.sched_boost
+	echo 0 >/dev/stune/top-app/schedtune.prefer_idle
+	echo 0 >/dev/stune/foreground/schedtune.prefer_idle
 else
 	if grep "msm8998" /system/build.prop || grep "msm8998" /vendor/build.prop; then
 		#LITTLE
-		echo 83 883200:85 1171200:87 1324800:91 1555200:95 > /sys/devices/system/cpu/cpufreq/policy0/interactive/target_loads
+		echo 82 883200:86 1171200:89 1324800:92 1555200:95 > /sys/devices/system/cpu/cpufreq/policy0/interactive/target_loads
 		echo 90000 > /sys/devices/system/cpu/cpufreq/policy0/interactive/timer_slack
 		echo 20000 > /sys/devices/system/cpu/cpufreq/policy0/interactive/timer_rate
 		echo 1248000 > /sys/devices/system/cpu/cpufreq/policy0/interactive/hispeed_freq
@@ -31,27 +38,27 @@ else
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy0/interactive/boost
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy0/interactive/fast_ramp_down
 		echo 1 > /sys/devices/system/cpu/cpufreq/policy0/interactive/use_sched_load
-		echo 50000 > /sys/devices/system/cpu/cpufreq/policy0/interactive/boostpulse_duration
+		echo 0 > /sys/devices/system/cpu/cpufreq/policy0/interactive/boostpulse_duration
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy0/interactive/io_is_busy
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy0/interactive/enable_prediction
 		#big
-		echo 84 979200:86 1344000:88 1574400:91 1804800:95 > /sys/devices/system/cpu/cpufreq/policy4/interactive/target_loads
+		echo 85 979200:87 1344000:90 1574400:93 1804800:96 > /sys/devices/system/cpu/cpufreq/policy4/interactive/target_loads
 		echo 90000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/timer_slack
 		echo 1574400 > /sys/devices/system/cpu/cpufreq/policy4/interactive/hispeed_freq
 		echo 20000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/timer_rate
-		echo 0 1056000:20000 1574400:60000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/above_hispeed_delay
+		echo 0 1056000:40000 1574400:60000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/above_hispeed_delay
 		echo 400 > /sys/devices/system/cpu/cpufreq/policy4/interactive/go_hispeed_load
-		echo 10000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/min_sample_time
+		echo 5000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/min_sample_time
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy4/interactive/max_freq_hysteresis
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy4/interactive/boost
 		echo 1 > /sys/devices/system/cpu/cpufreq/policy4/interactive/fast_ramp_down
 		echo 1 > /sys/devices/system/cpu/cpufreq/policy4/interactive/use_sched_load
-		echo 50000 > /sys/devices/system/cpu/cpufreq/policy4/interactive/boostpulse_duration
+		echo 0 > /sys/devices/system/cpu/cpufreq/policy4/interactive/boostpulse_duration
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy4/interactive/io_is_busy
 		echo 0 > /sys/devices/system/cpu/cpufreq/policy4/interactive/enable_prediction
 	elif grep "msm8996" /system/build.prop || grep "msm8996" /vendor/build.prop; then
 		#LITTLE
-		echo 76 729600:81 844800:84 1228800:86 1324800:92 1478400:99 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+		echo 78 729600:81 844800:84 1228800:86 1324800:92 1478400:99 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
 		echo 90000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack
 		echo 1228800 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
 		echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
@@ -62,7 +69,7 @@ else
 		echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
 		echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/boostpulse_duration
 		#big
-		echo 78 729600:80 806400:84 1248000:88 1324800:90 1785600:96 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/target_loads
+		echo 80 806400:84 1248000:88 1324800:90 1785600:96 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/target_loads
 		echo 90000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/timer_slack
 		echo 1248000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/hispeed_freq
 		echo 20000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/timer_rate
@@ -135,7 +142,8 @@ else
 		echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
 		echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/boostpulse_duration
 	else
-		echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/boost
+		echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/boost
+		echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
 	fi
 fi
 
@@ -169,19 +177,15 @@ echo 1 > /proc/sys/net/ipv4/tcp_window_scaling
 #WQ
 echo Y > /sys/module/workqueue/parameters/power_efficient
 
-##FS
-echo 10 > /proc/sys/fs/lease-break-time
-echo 32768 > /proc/sys/fs/inotify/max_queued_events
-echo 256 > /proc/sys/fs/inotify/max_user_instances
-echo 16384 > /proc/sys/fs/inotify/max_user_watches
-
 #LMK
 chown root /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 if grep "OnePlus" /system/build.prop; then
-	echo "9216,11520,13824,16128,27648,47040" > /sys/module/lowmemorykiller/parameters/minfree
-else
-	echo "16384,20480,24576,28672,49152,70560" > /sys/module/lowmemorykiller/parameters/minfree
+	echo "18432,23040,27648,51256,84224,121856" > /sys/module/lowmemorykiller/parameters/minfree
+elif grep "HTC" /system/build.prop; then
+	echo "27648,41472,48384,72192,84224,121856" > /sys/module/lowmemorykiller/parameters/minfree
+else	
+	echo "18432,23040,27648,51256,150295,200640" > /sys/module/lowmemorykiller/parameters/minfree
 fi
 echo 0 > /sys/module/lowmemorykiller/parameters/debug_level
 
@@ -199,26 +203,26 @@ if [ -e /sys/block/zram0 ]; then
 	echo 64 > /sys/block/zram0/queue/nr_requests
 	echo 4 > /sys/block/zram0/max_comp_streams
 	chmod 644 /sys/block/zram0/disksize
-	echo 1073741824 > /sys/block/zram0/disksize
+	echo 536870912 > /sys/block/zram0/disksize
 	mkswap /dev/block/zram0 > /dev/null 2>&1
 	swapon /dev/block/zram0 > /dev/null 2>&1
 fi
 
 #VM
-echo 1500 > /proc/sys/vm/dirty_expire_centisecs
+echo 1000 > /proc/sys/vm/dirty_expire_centisecs
 echo 1500 > /proc/sys/vm/dirty_writeback_centisecs
 echo 0 > /proc/sys/vm/oom_kill_allocating_task
-echo 3 > /proc/sys/vm/page-cluster
-echo 30 > /proc/sys/vm/swappiness
+echo 2 > /proc/sys/vm/page-cluster
+echo 20 > /proc/sys/vm/swappiness
 echo 70 > /proc/sys/vm/vfs_cache_pressure
 echo 15 > /proc/sys/vm/dirty_ratio
 echo 10 > /proc/sys/vm/dirty_background_ratio
-echo 0 > /proc/sys/vm/overcommit_memory
+echo 1 > /proc/sys/vm/overcommit_memory
 echo 80 > /proc/sys/vm/overcommit_ratio
 if grep "OnePlus" /system/build.prop; then
-	echo 20480 > /proc/sys/vm/min_free_kbytes
+	echo 11088 > /proc/sys/vm/min_free_kbytes
 else
-	echo 10240 > /proc/sys/vm/min_free_kbytes
+	echo 7392 > /proc/sys/vm/min_free_kbytes
 fi
 echo 64 > /proc/sys/kernel/random/read_wakeup_threshold
 echo 896 > /proc/sys/kernel/random/write_wakeup_threshold
