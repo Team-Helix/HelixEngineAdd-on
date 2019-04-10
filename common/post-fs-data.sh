8cc74ad2dcf3f6754bb511b2aaab81091d1390a3
RogerF81 <lowhouse123@gmail.com>
2019-04-10 21:39:34 +0200

    Use some commands in post-fs mode
    Just to be sure

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -7,3 +7,19 @@
 MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
+
+#Disable BCL
+if [ -e "/sys/devices/soc/soc:qcom,bcl/mode" ]; then
+	chmod 644 /sys/devices/soc/soc:qcom,bcl/mode
+	echo -n disable > /sys/devices/soc/soc:qcom,bcl/mode
+fi
+
+#Stopping perfd
+stop perfd
+
+# Disable sysctl.conf to Prevent ROM Interference
+if [ -e /system/etc/sysctl.conf ]; then
+  mount -o remount,rw /system
+  mv /system/etc/sysctl.conf /system/etc/sysctl.conf.bak
+  mount -o remount,ro /system
+fi
